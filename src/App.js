cf32fe27e0fe65f2c0b1ae4fdafc6ff93a0b4071
mansi <ladkarmansi@gmail.com>
2022-02-19 00:47:34 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {
+https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false
+
+https: function App() {
   return <div>hi</div>;
 }
 
