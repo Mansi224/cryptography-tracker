536e28f1834df66acc2edf1dc34c49dca3b15622
mansi <ladkarmansi@gmail.com>
2022-03-03 14:40:04 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 import "./App.css";
 
-https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 https: function App() {
+const [coins, setCoins] = useState([];)
+
+ useEffect(() => {
+   axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false
+').then(res => {
+  setCoins(res.data);
+})
+.catch(error => console.log(error));
+ }, []);
+
   return <div>hi</div>;
 }
 
